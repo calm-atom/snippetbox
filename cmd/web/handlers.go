dd9706b4585a374db6ddee0eec9363f9c446cb7f
John Buckley <jbuckleyv@gmail.com>
2023-11-03 07:25:15 -0500

    Changed to different router, added handler for post

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,14 +8,10 @@ import (
 	"strconv"
 
 	"github.com/calm-atom/snippetbox/internal/models"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
@@ -33,7 +29,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -56,53 +54,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
-// func (app *application) home(w http.ResponseWriter, r *http.Request) {
-// 	if r.URL.Path != "/" {
-// 		app.notFound(w)
-// 		return
-// 	}
-//
-// 	snippets, err := app.snippets.Latest()
-// 	if err != nil {
-// 		app.serverError(w, r, err)
-// 		return
-// 	}
-//
-// 	// Use the new render helper.
-// 	app.render(w, r, http.StatusOK, "home.tmpl", templateData{
-// 		Snippets: snippets,
-// 	})
-// }
-// func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-// 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-// 	if err != nil || id < 1 {
-// 		app.notFound(w)
-// 		return
-// 	}
-//
-// 	snippet, err := app.snippets.Get(id)
-// 	if err != nil {
-// 		if errors.Is(err, models.ErrNoRecord) {
-// 			app.notFound(w)
-// 		} else {
-// 			app.serverError(w, r, err)
-// 		}
-// 		return
-// 	}
-//
-// 	// Use the new render helper.
-// 	app.render(w, r, http.StatusOK, "view.tmpl", templateData{
-// 		Snippet: snippet,
-// 	})
-// }
-
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form to create a snippet here..."))
+}
 
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	title := "0 snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := 7
@@ -113,6 +69,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
 }
