dd338aa8066b3c57ebf0da6aecd5fa655c89cfb0
John Buckley <jbuckleyv@gmail.com>
2023-11-13 10:54:35 -0600

    Added tests

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -19,6 +19,12 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
+type SnippetModelInterface interface {
+	Insert(title string, content string, expires int) (int, error)
+	Get(id int) (Snippet, error)
+	Latest() ([]Snippet, error)
+}
+
 // Insert a snippet
 func (m *SnippetModel) Insert(title, content string, expires int) (int, error) {
 	stmt := `INSERT INTO snippets (title, content, created, expires)
